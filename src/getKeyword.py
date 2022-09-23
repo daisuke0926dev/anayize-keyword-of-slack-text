0bb1b845d9712d727621d4a1337d4cf306e47acd
Daisuke_Ishii <dev_daisuke0926@yahoo.co.jp>
2022-09-23 14:21:07 +0900

    make draw.py

--- src/getKeyword.py
+++ src/getKeyword.py
@@ -1,11 +1,17 @@
+from array import array
 import pke
 import separatingWords
 
-extractor = pke.unsupervised.MultipartiteRank()
 
-separated_words_massage_array = separatingWords.separete()
-for separated_words_massage in separated_words_massage_array:
-    extractor.load_document(input=separated_words_massage, language='ja', normalization=None)
-    extractor.candidate_selection(pos={'NOUN', 'PROPN', 'ADJ', 'NUM'})
-    extractor.candidate_weighting(threshold=0.74, method='average', alpha=1.1)
-    print(extractor.get_n_best(3))
+def getKeywordAndImportance():
+    extractor = pke.unsupervised.MultipartiteRank()
+    # for return
+    word_and_importance:array = []
+
+    separated_words_massage_array = separatingWords.separete()
+    for separated_words_massage in separated_words_massage_array:
+        extractor.load_document(input=separated_words_massage, language='ja', normalization=None)
+        extractor.candidate_selection(pos={'NOUN', 'PROPN', 'ADJ', 'NUM'})
+        extractor.candidate_weighting(threshold=0.74, method='average', alpha=1.1)
+        word_and_importance.append(extractor.get_n_best(3))
+    return word_and_importance
